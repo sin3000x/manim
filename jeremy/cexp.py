1c52b100f8940e9365719b9c3705e0be2bd4ef54
sin3000x@gmail.com <jiangjing@Jeremy-2.local>
2021-02-05 18:05:01 +0800

    finish cexp teacher scene

---
+++ jeremy/cexp.py
@@ -0,0 +1,6 @@
+from manimlib.imports import *
+
+class ComplexNotation(TeacherStudentsScene):
+    def construct(self):
+        self.teacher_says("约定$z\\in\\C$\\\\$x,y\\in\\R$", run_time=5)
+        self.wait(8)
