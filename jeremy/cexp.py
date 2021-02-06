20c5a02e1dcec0891db89e20935e8d29999bedcc
sin3000x@gmail.com <jiangjing@Jeremy-2.local>
2021-02-06 10:09:18 +0800

    start cexp

--- jeremy/cexp.py
+++ jeremy/cexp.py
@@ -1,1 +1,8 @@
 from manimlib.imports import *
+
+m = {"z": YELLOW}
+
+class Series(Scene):
+    def construct(self):
+        exp = Tex(r"\e^{z} \coloneqq 1+ z + {z ^2 \over 2!} + {z ^3 \over 3!} + \cdots", isolate="z").set_color_by_tex_to_color_map(m)
+        self.add(exp)
--- jeremy/test_mac.py
+++ jeremy/test_mac.py
@@ -2,12 +2,5 @@ from manimlib.imports import *
 
 class SquareToCircle(Scene):
     def construct(self):
-        circle = Circle()
-        circle.set_fill(BLUE, opacity=0.5)
-        circle.set_stroke(BLUE_E, width=4)
-        square = Square()
-
-        self.play(ShowCreation(square))
-        self.wait()
-        self.play(ReplacementTransform(square, circle))
-        self.wait()
+        l = Logo()
+        self.play(ShowCreation(l), run_time=8)
